e8bf0b5bf886d981d0ebc42b603fe3adebcc5e88
Rossnhi <52858656+Rossnhi@users.noreply.github.com>
2026-05-17 23:51:34 +0530

    Add: sliding-window-maximum - submission-6

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = [0] * (len(nums) - k + 1)
+        right = [0] * len(nums)
+        left = [0] * len(nums)
+        for i in range(len(nums)):
+            if i % k == 0:
+                left[i] = nums[i]
+            else:
+                left[i] = max(left[i - 1], nums[i])
+            if i == 0 or (len(nums) - i - 1) % k == 0:
+                right[-i - 1] = nums[-i - 1]
+            else:
+                right[-i - 1] = max(right[-i], nums[-i - 1])
+        for i in range(len(nums) - k + 1):
+            res[i] = max(right[i], left[i + k - 1])
+        return res
